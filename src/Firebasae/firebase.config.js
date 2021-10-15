e4fb3331e0d6f7f56ccfcbe4add3c323fb090c04
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2021-10-15 16:34:16 +0600

    first commit

---
+++ src/Firebasae/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcUrCwWjZzUY4M58UqVbAuPOkgiPu2n2Y",
+    authDomain: "ema-john-simple-cc07d.firebaseapp.com",
+    projectId: "ema-john-simple-cc07d",
+    storageBucket: "ema-john-simple-cc07d.appspot.com",
+    messagingSenderId: "394659935709",
+    appId: "1:394659935709:web:9085d0662d1f252e75b234"
+};
+
+export default firebaseConfig;
